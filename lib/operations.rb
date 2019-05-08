75bcf029fe5cbfe4cbc4e423b018706af9cf4ce0
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-08 18:24:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+	if speed < 40
+			puts "unsafe"
+		elsif
+			speed > 60
+			puts "unsafe"
+	else
+			puts "safe"
 end
 
-
+unsafe?(41)
 
 def not_safe?(speed)
-	
+
+	puts speed < 40 || speed > 60 ? "unsafe" : "safe"
+
 end
-	
 
-
+unsafe(50)
+not_safe?(20)
