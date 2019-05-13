fab813b1e594c882bc0ed75e49642062d1597a08
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-13 21:57:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,6 +7,6 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-  speed = gets.chomp
-  speed < 40 || speed >>> 60 : true ? false
+  speed = gets.chompdasa12
+  speed < 40 || speed > 60 : true ? false
 end
